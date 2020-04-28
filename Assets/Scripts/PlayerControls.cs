35cf3e7406361da1211b807274a9f52833862ee2
vyaaki <vyaaki1994@gmail.com>
2020-04-28 17:50:25 +0300

    Add script for player to control object. Change camera view.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private float speed = 20.0f;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * speed * moveX * Time.deltaTime);
+    }
+}
