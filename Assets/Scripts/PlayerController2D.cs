c601be39f4ed8373cbb786499427bc6fb93ba781
vyaaki <vyaaki1994@gmail.com>
2020-04-29 13:02:48 +0300

    Rename player controller script

---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    private float speed = 20.0f;
+    CharacterController controller;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * speed ;
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+}
