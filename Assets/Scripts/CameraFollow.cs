cfbfef555cdf7c9a6814281144d59005d527bc8c
vyaaki <vyaaki1994@gmail.com>
2020-04-30 16:04:21 +0300

    Add following for player camera script.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
+    private Vector3 curVelocity = Vector3.zero;
+    void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref curVelocity, smoothTime);
+    }
+}
