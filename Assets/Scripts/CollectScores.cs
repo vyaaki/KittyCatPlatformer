b65c913ca930e97d5c969cc2820b64d0fece7238
vyaaki <vyaaki1994@gmail.com>
2020-05-06 11:29:11 +0300

    Add script, empty sprite, sound, scripts and prefabs for collectable objects and score system.

---
+++ Assets/Scripts/CollectScores.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CollectScores : MonoBehaviour
+{
+    public AudioSource collectSound;
+    public int ScoreValue = 10;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        collectSound.Play();
+        UpdateScore.ScoreCounter += ScoreValue;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateScore : MonoBehaviour
+{
+    public Text UIScoreText;
+    public static int ScoreCounter = 0;
+    void Update()
+    {
+        UIScoreText.GetComponent<Text>().text = "Score:" + ScoreCounter;
+    }
+}
