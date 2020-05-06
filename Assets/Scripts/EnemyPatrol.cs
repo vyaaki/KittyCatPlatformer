2992935588d9fa9573604bfed49173a5122d80ab
vyaaki <vyaaki1994@gmail.com>
2020-05-06 13:25:43 +0300

    Add patrolling enemy's script. Add new collision layers. Add tags for scores and enemies.

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform groundDetector;
+    public float speed = 5f;
+    private bool movingRight = true;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundRaycastHit = Physics2D.Raycast(groundDetector.position, Vector2.down);
+        if (!groundRaycastHit.collider)
+        {
+            transform.Rotate(new Vector2(0, -180));
+        }
+    }
+}
